4070efef8c924bb0a3c38075fe77729356a71118
BirkJederstrom <63236277+BirkJederstrom@users.noreply.github.com>
2026-02-08 18:30:38 +0100

    Benchmark-1M-Pin.py added

---
+++ Benchmark/Benchmark-1M-Pin.py
@@ -0,0 +1,87 @@
+#This script is constructed to study and analyze the speed at which a simple password of 4-digits can be broken using simple algorithms
+#This script tests unsalted hashes, a predetermined salt for a hash, and a randomly generated salt for each hash
+#This script tests the following encryptions; MD5 and SHA3-512.
+#Argon2id is omited due to issues involving Windows 11 integration on the PC used
+
+#This script is for educational purposes
+#You may use this script for educational, analytical, and ethical hacking purposes
+#You may not use this script maliciously
+#The author takes no responsibility over any malicious use
+
+#Authored by Birk Jederstrom
+
+import hashlib, time, secrets
+
+pword = '9999999'
+
+#Unsalted Hash
+hashedMD5 = hashlib.md5(pword.encode()).hexdigest()
+
+#Predetermined Salt
+FixedSalt = 'foobar'
+
+#Salted Hash
+SaltedHashedMD5 = hashlib.md5((pword + FixedSalt).encode()).hexdigest()
+
+#Randomly generated Salt
+Salt = secrets.token_hex(16)
+
+#Randomly salted Hash
+RandomlySaltedhashedSHA3_512 = hashlib.sha3_512((pword + Salt).encode()).hexdigest()
+
+def findMD5Hash(hashedBenchmark):
+    start = time.time()
+
+    x = 0
+    for x in range(10000000):
+        D = '{:07}'.format(x)
+
+        if hashlib.md5(D.encode()).hexdigest() == hashedBenchmark:
+            end = time.time()
+            length = end - start
+            print("Pass found ", D)
+            break
+    return length
+
+def findSaltedMD5Hash(hashedBenchmark):
+    start = time.time()
+
+    x = 0
+    for x in range(10000000):
+        D = ('{:07}'.format(x) + FixedSalt)
+
+        if hashlib.md5(D.encode()).hexdigest() == hashedBenchmark:
+            end = time.time()
+            length = end - start
+            print("Pass + salt found: ", D)
+            password = D.replace(FixedSalt, "")
+            print("The Password: ", password)
+            print("The salt was: ", FixedSalt)
+            break
+    return length
+
+def findRandomlySaltedSHA3_512Hash(hashedBenchmark):
+    start = time.time()
+
+    x = 0
+    for x in range(10000000):
+        D = ('{:07}'.format(x) + Salt)
+
+        if hashlib.sha3_512(D.encode()).hexdigest() == hashedBenchmark:
+            end = time.time()
+            length = end - start
+            print("Pass + Salt found: ", D)
+            password = D.replace(Salt, "")
+            print("The Password: ", password)
+            print("The salt was: ", Salt)
+            break
+    return length
+
+MD5Time = findMD5Hash(hashedMD5)
+SaltedMD5Time = findSaltedMD5Hash(SaltedHashedMD5)
+RandomlySaltedSHA3_512Time = findRandomlySaltedSHA3_512Hash(RandomlySaltedhashedSHA3_512)
+
+print("----- RESULTS -----")
+print("MD5 time: ", MD5Time)
+print("Salted MD5 time: ", SaltedMD5Time)
+print("Randomly Salted SHA3-512 time: ", RandomlySaltedSHA3_512Time)
